13eafe055eff000803a474d1c2b1b15e1d9bcf11
Kranthi babu <kranthibabud1@gmail.com>
2022-01-21 11:51:09 +0530

    make a space-x application

---
+++ Space-x/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./Component/Header/Header";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
