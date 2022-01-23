b289f778712e3a4b6e986fcff7478ec45e66c4ec
Kranthi babu <kranthibabud1@gmail.com>
2022-01-23 12:10:34 +0530

    Display launch based on rout

--- Space-x/src/CustomRouter.jsx
+++ Space-x/src/CustomRouter.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Launches from "./Component/launches/Launches";
-import LaunchView from "./launchView/LaunchView";
+import WrappedLaunchView from "./launchView/LaunchView";
 
 function CustomRouter() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Launches />} />
-        <Route path="/launch/:flight_number" element={<LaunchView />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Launches />} />
+      <Route path="/launch/:flight_number" element={<WrappedLaunchView />} />
+    </Routes>
   );
 }
 
--- Space-x/src/launchView/LaunchView.jsx
+++ Space-x/src/launchView/LaunchView.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import axios from "axios";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import "./style.css";
 
 class LaunchView extends React.Component {
+  state = {
+    launch: {},
+  };
+  componentDidMount = () => {
+    const flightNo = this.props.router.params.flight_number;
+    this.getLaunch(flightNo);
+  };
+  getLaunch = (flightNo) => {
+    axios
+      .get("https://api.spacexdata.com/v3/launches/" + flightNo)
+      .then((response) => {
+        this.setState({
+          launch: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  LaunchAttribute = (title, key) => {
+    const value = this.state.launch[key];
+    return (
+      <div className="attribute">
+        <h1>{title} </h1>
+        <h1>{value}</h1>
+      </div>
+    );
+  };
+
   render() {
-    return <div>LunchView</div>;
+    console.log(this.state.launch);
+    return (
+      <div className="launchView">
+        {this.LaunchAttribute("MissionName", "mission_name")}
+        {this.LaunchAttribute("Flight Number", "flight_number")}
+        {this.LaunchAttribute("LaunchDate", "launch_date_local")}
+        <p>{this.state.launch.details}</p>
+      </div>
+    );
   }
 }
+const WrappedLaunchView = (props) => {
+  const location = useLocation();
+  let navigate = useNavigate();
+  const params = useParams();
 
-export default LaunchView;
+  return <LaunchView {...props} router={{ location, navigate, params }} />;
+};
+
+export default WrappedLaunchView;
